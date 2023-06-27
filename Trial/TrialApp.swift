e0214d3a2aa27cc35b1d75c3aa2d6f2a603adacb
Yesha <yeshashah0021@gmail.com>
2023-06-27 14:34:45 -0400

    Initial Commit

---
+++ Trial/TrialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrialApp.swift
+//  Trial
+//
+//  Created by Yesha Shah on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct TrialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
